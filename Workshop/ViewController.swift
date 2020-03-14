c0d15da139670197c7fa9ea62659633cf89d236d
Jorge Torres <jorge.torres@globant.com>
2020-03-14 07:14:16 -0600

    Initial Commit

---
+++ Workshop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Workshop
+//
+//  Created by Jorge Armando Torres Perez on 3/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WorkshopTests/WorkshopTests.swift
@@ -0,0 +1,33 @@
+//
+//  WorkshopTests.swift
+//  WorkshopTests
+//
+//  Created by Jorge Armando Torres Perez on 3/14/20.
+//
+
+import XCTest
+@testable import Workshop
+
+class WorkshopTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
